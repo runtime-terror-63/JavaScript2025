f84c568e887a412e5eadabb23c9337ce18532e25
Sayan <sayandutta84314@gamil.com>
2024-11-28 13:28:46 +0530

    ok

---
+++ 04_datatypes_summary.js
@@ -0,0 +1,20 @@
+//stack (primitive), heap (non_premitive)
+let myYoutubename = "sayanduttadotcom";
+
+let anothername = myYoutubename;
+
+anothername = "chaiorcode";
+
+console.log(myYoutubename); //sayanduttadotcom
+console.log(anothername); //chaiorcode
+
+let user = {
+  email: "sayandutta84314@gmail.com",
+  upi: "user@bl",
+};
+
+let userTwo = user;
+userTwo.email = "hitest@google.com";
+
+console.log(user.email);
+console.log(userTwo.email);

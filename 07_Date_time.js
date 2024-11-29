1de62b21d9d885af5a68b53d6d519e0f922060e8
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-11-29 08:17:55 +0530

    Update 07_Date_time.js

--- 07_Date_time.js
+++ 07_Date_time.js
@@ -1,14 +1,14 @@
 //Date
 
-// let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toTimeString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(typeof myDate);
+ let myDate = new Date();
+ console.log(myDate.toString());
+ console.log(myDate.toLocaleString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toTimeString());
+ console.log(myDate.toJSON());
+ console.log(myDate.toLocaleDateString());
+ console.log(myDate.toLocaleTimeString());
+ console.log(typeof myDate);
 
 let myCreateDate = new Date(2023, 0, 23, 6, 8);
 console.log(myCreateDate.toDateString());

fe908341674a963867a39aec7c9819731a028eaa
Sayan <sayandutta84314@gamil.com>
2024-11-29 07:42:10 +0530

    maths

---
+++ 06_Number_maths.js
@@ -0,0 +1,29 @@
+/*const score = 400;
+console.log(score); //400
+
+const balance = new Number(100);
+console.log(balance); // [Number : 100]
+
+console.log(balance.toString().length); //3
+console.log(balance.toString());
+100;
+
+console.log(balance.toFixed(2));
+130.0;
+
+const otherNum = 129.535;
+console.log(otherNum.toPrecision(3)); //130
+
+const hundred = 1000000;
+console.log(hundred.toLocaleString("en-IN")); //10,00,000  */
+
+//+++++++++++++++++++++++++++++++++++++++++++++  Maths ==================
+
+console.log(Math);
+
+console.log(Math.random() * 10);
+console.log(Math.min(2, 3, 4, 7));
+console.log(Math.max(5, 6, 9, 1, 2));
+console.log(Math.round(4.7));
+console.log(Math.ceil());
+console.log(Math.floor(4.2));

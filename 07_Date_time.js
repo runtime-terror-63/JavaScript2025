509229f793be1315ca6a5b2c5d32632896d3052b
Sayan <sayandutta84314@gamil.com>
2024-11-29 08:26:56 +0530

    ok

--- 07_Date_time.js
+++ 07_Date_time.js
@@ -1,4 +1,5 @@
 //Date
+/*
 
  let myDate = new Date();
  console.log(myDate.toString());
@@ -16,6 +17,13 @@ console.log(myCreateDate.toLocaleString());
 
 let mylocalDate = new Date("01-14-2023");
 console.log(mylocalDate.toLocaleString())
-
+*/
 let myTimeStamp = Date.now();
-console.log(myTimeStamp);
+console.log(myTimeStamp); 
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date();
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth()+1)
+console.log(newDate.getTime())
